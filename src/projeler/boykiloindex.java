5d4e2d8a844a22e4e48a256d1930c5c994540656
Mehmet <mehmetkerem2109@gmail.com>
2025-03-03 20:16:44 +0300

    Initial commit

---
+++ src/projeler/boykiloindex.java
@@ -0,0 +1,19 @@
+package projeler;
+
+import java.util.Scanner;
+
+public class boykiloindex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen Boyunuzu Giriniz (Örnek : 1,75) : ");
+        double height = scanner.nextDouble();
+
+        System.out.print("Lütfen Kilonuzu Giriniz : ");
+        double weight = scanner.nextDouble();
+
+        double bmi = weight /(height * height);
+
+        System.out.println("Vücut Kitle İndeksiniz : "+ bmi);
+    }
+}
