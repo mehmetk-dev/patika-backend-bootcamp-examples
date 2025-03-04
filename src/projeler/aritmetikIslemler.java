00fa443bf48aa9e9540db2e75ab4e9f59c51e0e6
Mehmet <mehmetkerem2109@gmail.com>
2025-03-04 14:43:30 +0300

    yeni projeler eklendi

--- src/projeler/aritmetikIslemler.java
+++ src/projeler/aritmetikIslemler.java
@@ -6,24 +6,14 @@ public class aritmetikIslemler {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        //Kullanıcıdan 3 tane değişken alıyoruz.
         System.out.print("A,B,C Sayılarını Sırayla Giriniz :");
         int numA = scanner.nextInt();
         int numB = scanner.nextInt();
         int numC = scanner.nextInt();
 
-        System.out.println("İşlem sonucu : " + (((numA + numB) * numC)- numB));
+        // Soruda istendiği gibi işlem önceliğine göre hesaplama yapıyoruz
+        // İşlem sırası : a + b * c - b
+        System.out.println("İşlem sonucu : " + (((numA + numB) * numC) - numB));
     }
-    /*
-        Soru: Kullanıcıdan 3 tane sayı (a, b, c) alıp bu sayıları işlem sırasına göre işleyen bir program yazın.
-
-        İşlem sırası şöyle olsun: a+b*c-b. İşlem tamamladıktan sonra sonucu ekrana yazdırın.
-
-        Örnek: Kullanıcıdan alınan sayılar 10, 2 ve 3 olsun. Yapılacak işlem ise 10+2*3-2 şeklinde olsun.
-
-        İşlemleri kullanıcı girmeyecek olup kodunuzda yazmış olmalısınız.
-
-        İpucu 1: Kullanıcıdan veri almak için `Scanner` sınıfını kullanabilirsiniz.
-
-        İpucu 2: Sonucu ekrana yazdırmak için `System.out.println()` metodunu kullanabilirsiniz.
- */
 }
