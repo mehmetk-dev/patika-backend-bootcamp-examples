51684c1745566b842ab91f8aa573995b0a8459f6
Mehmet <mehmetkerem2109@gmail.com>
2025-03-04 14:51:21 +0300

    proje güncellendi

--- src/projeler/artikYil.java
+++ src/projeler/artikYil.java
@@ -10,6 +10,13 @@ public class artikYil {
         System.out.println("Artık yıl hesaplaması yapmak istediğiniz yılı giriniz: ");
         int year = scanner.nextInt();
 
+        /*Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+
+        1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+        100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+        Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir. */
+
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ){
             System.out.println(year + " Artık yıldır.");
         }
