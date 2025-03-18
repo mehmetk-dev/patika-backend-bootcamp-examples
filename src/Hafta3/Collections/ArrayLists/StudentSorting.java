db68f59f052930bd90540ed3ecb3e4bccecd702c
Mehmet <mehmetkerem2109@gmail.com>
2025-03-18 03:29:00 +0300

    ArrayList ve LinkedList örnekleri eklendi.

---
+++ src/Hafta3/Collections/ArrayLists/StudentSorting.java
@@ -0,0 +1,43 @@
+package Hafta3.Collections.ArrayLists;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Student {
+    String name;
+    double grade;
+
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - Not Ortalaması: " + grade;
+    }
+}
+
+public class StudentSorting {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+
+        students.add(new Student("Ahmet", 85.5));
+        students.add(new Student("Zeynep", 92.0));
+        students.add(new Student("Mehmet", 76.3));
+        students.add(new Student("Elif", 89.4));
+
+        System.out.println("Orijinal Liste:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+
+        // Not ortalamasına göre büyükten küçüğe sıralama
+        students.sort(Comparator.comparingDouble(s -> s.grade));
+
+        System.out.println("\nNot Ortalamasına Göre (Büyükten Küçüğe) Sıralanmış Liste:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/Hafta3/Collections/LinkedList/LinkedListTest.java
@@ -0,0 +1,15 @@
+package Hafta3.Collections.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+
+        LinkedList<String> isimListesi = new LinkedList<>();
+        List<String> isimListesi2 = new LinkedList<>();
+
+        isimListesi.add("Mehmet");
+        isimListesi.add("Kerem");
+    }
+}
