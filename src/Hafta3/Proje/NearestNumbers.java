f490b5968896826cc2a18375f2506d083997ee7d
Mehmet <mehmetkerem2109@gmail.com>
2025-03-18 17:47:30 +0300

    Ödev Yüklendi.

--- src/Hafta3/Proje/NearestNumbers.java
+++ src/Hafta3/Proje/NearestNumbers.java
@@ -28,13 +28,12 @@ public class NearestNumbers {
     public static void findClosestPair(ArrayList<Integer> numbers){
         Collections.sort(numbers); // Numbers listesini sıralıyoruz.
 
-
         int minDifference = Integer.MAX_VALUE; // Farkı en başta en büyük alıyoruz ki diff değişkeni ne olursa olsun güncellesin diye.
         int closestPairFirst = -1, closestPairSecond = -1;
 
         for (int i = 0; i < numbers.size() - 1; i++) {
             int diff = numbers.get(i+1) - numbers.get(i); //diff değişkeni numbers'in 1. ve 2. elemanının birbirinden farkını tutar.
-            if (diff < minDifference){
+            if (diff < minDifference){//Eğer sıradaki fark daha azsa sayıları ve farkı günceller
                 minDifference = diff;
                 closestPairFirst = numbers.get(i);
                 closestPairSecond = numbers.get(i+1);
