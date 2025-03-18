f04c2c8ec05aadc04634d29fb42ece7f10da8d35
Mehmet <mehmetkerem2109@gmail.com>
2025-03-18 14:42:40 +0300

    ArrayList ve LinkedList örnekleri eklendi.

---
+++ src/Hafta3/Collections/LinkedList/StackData.java
@@ -0,0 +1,26 @@
+package Hafta3.Collections.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StackData {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> stack = new LinkedList<>();
+
+        //eleman ekleme
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println(stack);
+
+        //eleman çıkarma
+        int removedElement = stack.pop();
+        System.out.println("Çıkartılan eleman: " + removedElement);
+        System.out.println("Stack durumu: " + stack);
+
+        // Stack'in en üstündeki elemanı görmek (peek)
+        int topElement = stack.peek();
+        System.out.println("Stack'in en üstündeki eleman: " + topElement);
+    }
+}
