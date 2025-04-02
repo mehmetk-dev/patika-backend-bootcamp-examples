217839d34d4a73d27750d748d7536f7585feeeee
Mehmet <mehmetkerem2109@gmail.com>
2025-04-02 19:34:48 +0300

    5.Haftaya Geçildi.

--- src/Hafta5/Class.java
+++
@@ -1,4 +0,0 @@
-package Hafta5;
-
-public class Class {
-}
---
+++ src/Hafta5/Proje/EnumExample/WorkSchedule.java
@@ -0,0 +1,21 @@
+package Hafta5.Proje.EnumExample;
+
+import java.util.Scanner;
+
+public class WorkSchedule {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bir gün giriniz(Örneğin: MONDAY):");
+        String inputDay = scanner.nextLine().toUpperCase();
+
+        //Alınan değerin kontrolü
+        if (Day.isValidDay(inputDay)) {
+            Day day = Day.valueOf(inputDay);
+            System.out.println(inputDay + " için çalışma saati " + day.getWorkHours());
+        } else {
+            System.out.println("Geçersiz bir gün girdiniz.");
+        }
+    }
+
+}
