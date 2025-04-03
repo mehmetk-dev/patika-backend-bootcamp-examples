bf9568e6e0cc0813997cfce3854670433db18dc4
Mehmet <mehmetkerem2109@gmail.com>
2025-04-04 01:34:47 +0300

    5.Haftaya Geçildi.

--- src/Hafta5/Proje/Employee.java
+++ src/Hafta5/Proje/Employee.java
@@ -6,37 +6,38 @@ public class Employee {
     private String lastName;
     private ContactInfo contactInfo;
 
-    //Constructor
+    //Constructor to initialize employee's name and contact info
     public Employee(String firstName, String lastName,String email,String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.contactInfo = new ContactInfo(phone,email);
     }
 
-    //Inner Class
+    // Inner Class to store contact information
     public class ContactInfo{
         private String phoneNumber;
         private String email;
 
-        //Inner Class Constructor
+        //Constructor to initialize contact details
         public ContactInfo(String phoneNumber, String email) {
             this.phoneNumber = phoneNumber;
             this.email = email;
         }
 
-        //Method returning contact information
+        // Method to display contact information
         public void displayInfo(){
             System.out.println("Phone: " + phoneNumber);
             System.out.println("Email: " + email);
         }
     }
 
-    //Method to show the employee contact details
+    // Method to display employee's name and contact info
     public void showContatInfo(){
         System.out.println("Employee: " + firstName + " " + lastName);
         contactInfo.displayInfo();
     }
 
+    // Main method to test the structure
     public static void main(String[] args) {
         Employee emp = new Employee("Mehmet","Kerem","mehmetkerem@gmail.com","05340711077");
         emp.showContatInfo();
