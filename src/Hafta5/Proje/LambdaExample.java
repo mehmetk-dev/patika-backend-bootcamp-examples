4a8532dc3eb924081f5982d6f51c27eac16b5362
Mehmet <mehmetkerem2109@gmail.com>
2025-04-05 20:20:19 +0300

    Ödev Eklendi.

---
+++ src/Hafta5/Proje/LambdaExample.java
@@ -0,0 +1,40 @@
+package Hafta5.Proje;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+//        // Sayıları iki katına çıkarma (Geleneksel yöntem)
+//        for (int i = 0; i < numbers.size(); i++) {
+//            numbers.set(i, numbers.get(i) * 2);
+//        }
+//        // Sonuçları yazdırma
+//        for (int number : numbers) {
+//           System.out.println(number);
+//        }
+
+        //Function<Integer,Integer> square = x -> x * x;
+        Function<Integer,Double> square = x -> Math.pow(x,2);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(square.apply(numbers.get(i)));
+        }
+
+    }
+
+
+
+
+
+
+}
+
+
