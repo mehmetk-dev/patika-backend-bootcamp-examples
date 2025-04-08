5ec579c4dcbb8c68e660b0d43914711b7b2ae805
Mehmet <mehmetkerem2109@gmail.com>
2025-04-09 00:13:59 +0300

    Alıştırma Yüklendi.

---
+++ src/Hafta3/Proje/Customer/Customer.java
@@ -0,0 +1,84 @@
+package Hafta3.Proje.Customer;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private int customer_Id;
+    private Order order;
+
+
+    static int count = 1;
+
+    public Customer(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.customer_Id = count;
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCustomer_Id() {
+        return customer_Id;
+    }
+
+    public void setCustomer_Id(int customer_Id) {
+        this.customer_Id = customer_Id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", addres='" + address + '\'' +
+                ", customer_Id=" + customer_Id +
+                ", orderList=" + order +
+                '}';
+    }
+}
---
+++ src/Hafta3/Proje/Customer/OrderService.java
@@ -0,0 +1,30 @@
+package Hafta3.Proje.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderService {
+
+    List<Order> orderList = new ArrayList<>();
+    public String newOrderStatus = "Waiting";
+    public String paidStatus = "Paid";
+
+    public void addOrder(String product, double price,Customer customer){
+
+        Order order = new Order(product,price,newOrderStatus,customer);
+
+        customer.setOrder(order);
+        orderList.add(order);
+    }
+    public void listOrder(){
+
+        for (Order order : orderList){
+            System.out.println(order);
+        }
+    }
+
+    public void updatePayment(Order order){
+        order.setPaymentStatus(paidStatus);
+    }
+
+}
