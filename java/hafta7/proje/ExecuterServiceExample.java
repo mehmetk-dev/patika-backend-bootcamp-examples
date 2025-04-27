99d06755ac217c2c924af5506acef3bb9cc6b6bf
Mehmet <mehmetkerem2109@gmail.com>
2025-04-27 23:05:37 +0300

    Week-7 Thread Example Uploaded.

---
+++ java/hafta7/proje/ExecuterServiceExample.java
@@ -0,0 +1,25 @@
+package hafta7.proje;
+
+public class ExecuterServiceExample implements Runnable{
+
+    private String name;
+    private int taskId;
+
+    public ExecuterServiceExample(String name, int taskId) {
+        this.name = name;
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Worker " + name + " " + taskId + ".started work");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Worker " + name + " " + taskId + ".finished work");
+
+    }
+}
---
+++ java/hafta7/proje/ExecuterServiceMain.java
@@ -0,0 +1,43 @@
+package hafta7.proje;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecuterServiceMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        for (int i = 1; i <= 5; i++) {
+            executorService.submit(new ExecuterServiceExample(String.valueOf(i),i));
+        }
+
+        executorService.shutdown();
+
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+
+        /*Thread thread1 = new Thread(new Worker("1",1));
+        Thread thread2 = new Thread(new Worker("2",2));
+        Thread thread3 = new Thread(new Worker("3",3));
+        Thread thread4 = new Thread(new Worker("4",4));
+        Thread thread5 = new Thread(new Worker("5",5));
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        thread3.start();
+        thread4.start();
+
+        thread3.join();
+        thread4.join();
+
+        thread5.start();
+        */
+
+    }
+}
