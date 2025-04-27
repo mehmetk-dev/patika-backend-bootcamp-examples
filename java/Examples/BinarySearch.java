8d938c234dc11cf8c0b1f7d1ae78d344b1d92760
Mehmet <mehmetkerem2109@gmail.com>
2025-04-28 01:33:02 +0300

    Algorithm example uploaded.

---
+++ java/Examples/BinarySearch.java
@@ -0,0 +1,49 @@
+package Examples;
+
+import java.util.ArrayList;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] array ,int target){
+
+        int low = 0;
+        int high = array.length -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (array[mid] == target){
+                return mid;
+            } else if (array[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] array,int target,int low,int high){
+
+        if (low > high){
+            return  -1;
+        }
+
+        int mid = (low + high) + 2;
+
+        if (array[mid] == target){
+            return mid;
+        } else if(array[mid] < target){
+            return recursiveBinarySearch(array,target,low + 1 ,high);
+        } else {
+          return recursiveBinarySearch(array,target,low,high-1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1,6,123,544,3212,5433,13123};
+
+        System.out.println(binarySearch(numbers,544));
+    }
+}
---
+++ java/Examples/BubbleSort.java
@@ -0,0 +1,45 @@
+package Examples;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array){
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int[] bubbleSortBasic(int[] array){
+
+        for (int i = 0; i < array.length -1 ; i++) {
+            if (array[i] > array[i+1]){
+                int holder = array[i];
+                array[i] = array[i+1];
+                array[i+1] = holder;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12,10,8,14,6,2,3,1};
+//        for (int i = 0; i < numbers.length -1; i++) {
+//            bubbleSortBasic(numbers);
+//        }
+//        System.out.println(Arrays.toString(numbers));
+
+        bubbleSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+}
