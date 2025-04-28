551cfb72dbd576604bc3e63e63a7224eb60fdac5
Mehmet <mehmetkerem2109@gmail.com>
2025-04-28 23:56:15 +0300

    Thread examples upload.

--- java/hafta7/prework/threads/WaitAndNotifyExample.java
+++ java/hafta7/prework/threads/WaitAndNotifyExample.java
@@ -1,4 +1,38 @@
 package hafta7.prework.threads;
 
+import java.util.Scanner;
+
 public class WaitAndNotifyExample {
+
+    public void thread1Function() throws InterruptedException {
+
+        synchronized (this){
+            System.out.println("Thread 1 is working...");
+            System.out.println("Thread 1 is waiting for Thread 2 to wake up.");
+
+            wait();
+
+            System.out.println("Thread 1 is wake up.");
+        }
+    }
+
+
+
+    public void thread2Function() throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        Thread.sleep(2000);
+
+        synchronized (this){
+            System.out.println("Thread 2 is working...");
+
+            System.out.println("If you want to continue, press any key.");
+
+            scanner.nextLine();
+
+            notify();
+
+            System.out.println("I woke up Thread 1 and I'm leaving.");
+        }
+    }
 }
--- java/hafta7/prework/threads/WaitAndNotifyExampleMain.java
+++ java/hafta7/prework/threads/WaitAndNotifyExampleMain.java
@@ -1,4 +1,40 @@
 package hafta7.prework.threads;
 
 public class WaitAndNotifyExampleMain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WaitAndNotifyExample wn = new WaitAndNotifyExample();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    wn.thread1Function();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    wn.thread2Function();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
 }
