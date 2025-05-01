49ca6fbea09959b761b10b1d4759d50cd7bcd3ee
Mehmet <mehmetkerem2109@gmail.com>
2025-05-01 22:04:18 +0300

    Thread examples upload.

--- java/hafta7/prework/threads/Example.java
+++
@@ -1,8 +0,0 @@
-package hafta7.prework.threads;
-
-public class Example {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ java/hafta7/prework/threads/SemaphoreExample.java
@@ -0,0 +1,25 @@
+package hafta7.prework.threads;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreExample {
+
+    Semaphore semaphore = new Semaphore(3);
+
+    public void threadFonksiyonu(int i) {
+
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Semaphore starting... ID: " + i );
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        semaphore.release();
+    }
+}
