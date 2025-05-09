e61bb2c1372ebab13a4a0239b9a1f2d1fdbf4880
Mehmet <mehmetkerem2109@gmail.com>
2025-05-09 16:49:59 +0300

    JDBC examples added.

--- java/hafta8/jdbc/library/LibraryMain.java
+++ java/hafta8/jdbc/library/LibraryMain.java
@@ -1,4 +1,21 @@
 package hafta8.jdbc.library;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class LibraryMain {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:postgresql://localhost:5432/Library";
+        String username = "postgres";
+        String password = "mehmet619";
+
+        try(Connection connection = DriverManager.getConnection(url,username,password)){
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
