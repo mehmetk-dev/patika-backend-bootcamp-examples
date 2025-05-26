4dbe6b2c709124dd7f2d760388322a5e824a143a
Mehmet <mehmetkerem2109@gmail.com>
2025-05-26 19:27:45 +0300

    Hibernate examples added.

---
+++ java/hafta11/oneToMany/Customer.java
@@ -0,0 +1,49 @@
+package hafta11.oneToMany;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id",columnDefinition = "serial")
+    private int id;
+
+    @Column(name = "customer_name",nullable = false,length = 100)
+    private String name;
+
+    @Column(name = "customer_mail",nullable = false,unique = true)
+    private String mail;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "customer_gender")
+    private GENDER gender;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "customer_on_date")
+    private LocalDate onDate;
+
+    public enum GENDER{
+        MALE,
+        FEMALE
+    }
+
+    public Customer() {
+    }
+
+    public Customer(String name, String mail, GENDER gender, LocalDate onDate) {
+        this.name = name;
+        this.mail = mail;
+        this.gender = gender;
+        this.onDate = onDate;
+    }
+}
