583ae170aef749dfddc36d42cd15ee6fbf9c59b9
Mehmet <mehmetkerem2109@gmail.com>
2025-05-28 22:29:01 +0300

    Hibernate examples added.

--- java/hafta12/Runner.java
+++ java/hafta12/Runner.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -19,6 +21,50 @@ public class Runner {
         customer.setEmail("mehmetkerem");
 
         session.beginTransaction();
+
+        List<Customer> customers = session.createQuery("FROM Customer", Customer.class).getResultList();
+        for (Customer c : customers) {
+            System.out.println(c.getName() + " - " + c.getEmail());
+        }
+
+        List<Customer> adults = session.createQuery("FROM Customer WHERE age > 18", Customer.class).getResultList();
+
+        List<Customer> customers1 = session.createQuery("FROM Customer", Customer.class).getResultList();
+        for (Customer c : customers1) {
+            System.out.println(c.getName() + " - " + c.getEmail());
+        }
+
+        Customer c = session.createQuery("FROM Customer WHERE email = :email", Customer.class)
+                .setParameter("email", "mehmetkerem")
+                .uniqueResult();
+
+        List<String> names = session.createQuery("SELECT name FROM Customer", String.class).getResultList();
+
+
+        Long count = session.createQuery("SELECT COUNT(*) FROM Customer", Long.class).uniqueResult();
+        System.out.println("Toplam müşteri: " + count);
+
+
+        List<Customer> sorted = session.createQuery("FROM Customer ORDER BY age DESC", Customer.class).getResultList();
+
+
+
+        session.createQuery("UPDATE Customer SET email = :newEmail WHERE name = :name")
+                .setParameter("newEmail", "yeniemail@example.com")
+                .setParameter("name", "mehmet")
+                .executeUpdate();
+
+
+        session.createQuery("DELETE FROM Customer WHERE name = :name")
+                .setParameter("name", "mehmet")
+                .executeUpdate();
+
+
+        List<Object[]> results = session.createQuery("SELECT age, COUNT(*) FROM Customer GROUP BY age", Object[].class).getResultList();
+
+        for (Object[] row : results) {
+            System.out.println("Age: " + row[0] + ", Count: " + row[1]);
+        }
 
         session.persist(customer);
 
